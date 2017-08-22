abb6faed5c0a75e81ee97d3a9860467cd08658f0
Nate Chapman <chapman.nate@gmail.com>
2017-08-22 12:19:23 -0500

    Created auth routes under route_util

--- frontend/uTube.jsx
+++ frontend/uTube.jsx
@@ -4,7 +4,15 @@ import Root from './components/root';
 import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const store = configureStore();
+  
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
 
   // testing on window
   window.getState = store.getState;
