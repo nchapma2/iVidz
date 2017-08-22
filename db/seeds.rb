18688b5c5db72fd4fee640b357f6f47b1ebe17d0
Nate Chapman <chapman.nate@gmail.com>
2017-08-22 08:58:19 -0500

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+User.create(username:'MickeyMouse', password:'password1', email:'MickeyMouse@myemail.com')
+User.create(username:'DaffyDuck', password:'password2', email:'DaffyDuck@myemail.com')
+User.create(username:'LoneRanger', password:'password3', email:'LoneRanger@myemail.com')
+User.create(username:'SharkBoy', password:'password4', email:'SharkBoy@myemail.com')
+User.create(username:'JacknJill', password:'password5', email:'JacknJill@myemail.com')
