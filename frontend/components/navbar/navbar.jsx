ad883c475f0eb21908eec62c0c4cd74e7651a619
Nate Chapman <chapman.nate@gmail.com>
2017-08-23 17:45:08 -0400

    Added upload icon

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -55,7 +55,7 @@ class Navbar extends React.Component {
         placeholder='Search'
         />
       <Link to='/upload'>
-        <img alt='upload' className='upload-icon' src='upload-icon.png'></img>
+        <img alt='upload' className='upload-icon' src={window.upload}></img>
       </Link>
       { this.greeting() }
     </div>
